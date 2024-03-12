fd0787ac9aff8e869d1f51599157f998cddb4b25
Phat <tanphat2000hp@gmail.com>
2024-03-12 10:07:42 +0700

    first commit

---
+++ db/config.php
@@ -0,0 +1,7 @@
+<?php
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'test_banhang');
+
+define('MD5_PRIVATE_KEY', '09JJJjhh7834jHJG876312^&%shjdgsjagdasKoks');
---
+++ products.php
@@ -0,0 +1,24 @@
+<?php
+	require_once('db/dbhelper.php');
+	require_once('utils/utility.php');
+	include_once('layouts/header.php');
+
+	$productList = executeResult('select * from products');
+?>
+<!-- body -->
+<div class="container">
+	<div class="row">
+		<?php
+			foreach ($productList as $item) {
+				echo '<div class="col-md-3 col-6">
+						<a href="details.php?id='.$item['id'].'"><img src="'.$item['thumbnail'].'" style="width: 100%"></a>
+						<a href="details.php?id='.$item['id'].'"><p>'.$item['title'].'</p></a>
+						<p style="mau: red">'.number_format($item['price'], 0, ',', '.').'đ</p>
+					</div>';
+			}
+		?>
+	</div>
+</div>
+<?php
+	include_once('layouts/footer.php');
+?>
