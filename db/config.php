abee9fd749905f8f9fc7b237ce8c5f39681cd73d
Phat <tanphat2000hp@gmail.com>
2024-03-27 10:07:58 +0700

    add db.txt

--- db/config.php
+++ db/config.php
@@ -2,6 +2,6 @@
 define('HOST', 'localhost');
 define('USERNAME', 'root');
 define('PASSWORD', '');
-define('DATABASE', 'test_banhang');
+define('DATABASE', 'web_ban_hang');
 
 define('MD5_PRIVATE_KEY', '09JJJjhh7834jHJG876312^&%shjdgsjagdasKoks');
